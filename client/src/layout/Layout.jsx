3f95cdfa7cf106ed39f55acdee1c9ad51f589f73
Nils Hartmann <nils@nilshartmann.net>
2016-05-12 23:04:13 +0200

    Add section

--- client/src/layout/Layout.jsx
+++ client/src/layout/Layout.jsx
@@ -12,7 +12,7 @@ class Layout extends React.Component {
       // return <Link to={`/login${location.pathname}`}>Login</Link>;
       return null;
     } else {
-      return <span><a onClick={()=>logout()}>Logout</a> | <a onClick={()=>newPost()}>New Post</a> | <Link to='/upload'>Uploads</Link></span>;
+      return <span><a onClick={() => logout() }>Logout</a> | <a onClick={() => newPost() }>New Post</a> | <Link to='/upload'>Uploads</Link></span>;
     }
   }
 
@@ -43,24 +43,29 @@ class Layout extends React.Component {
                 <a className='ReadMore'  href='http://www.reactbuch.de' target='_blank'><img src='/uploads/react-buch-titel.jpg'/>React - Die praktische Einf&uuml;hrung in React, React Router und Redux (Juni 2016)</a>
               </div>
               <div className='Image'>
-                <Link className='ReadMore'  to='/posts/das-osgi-buch'><img  src='/uploads/die_osgi_service_platform_01.jpg'/>Die OSGi Service Platform (April 2008)</Link>
+                <Link className='ReadMore'  to='/posts/das-osgi-buch'><img  src='/uploads/die_osgi_service_platform_01.jpg'/>Die OSGi Service Platform (April 2008) </Link>
               </div>
             </div>
             <div className='Section'>
               <h1 className='Title'>Talks</h1>
               <div className='Image'>
-                <a className='ReadMore' href='https://jax.de/session/komponentenentwicklung-mit-react/' target='_blank'><img src='/uploads/JAX_2016_Speakerbutton_Rectangle.jpg'/>Komponentenentwicklung mit React (April 2016)</a>
+                <a className='ReadMore' href='https://jax.de/session/komponentenentwicklung-mit-react/' target='_blank'><img src='/uploads/JAX_2016_Speakerbutton_Rectangle.jpg'/>Komponentenentwicklung mit React (April 2016) </a>
               </div>
               <div className='Image'>
-                <a className='ReadMore' href='https://www.enterjs.de/abstracts.html#worksop-react' target='_blank'><img  src='/uploads/enterjs.svg'/>React Einsteiger Workshop (Juni 2016)</a>
+                <a className='ReadMore' href='https://www.enterjs.de/abstracts.html#worksop-react' target='_blank'><img  src='/uploads/enterjs.svg'/>React Einsteiger Workshop (Juni 2016) </a>
               </div>
             </div>
             <div className='Section'>
               <h1 className='Title'>Kolumne</h1>
               <div className='Image'>
-                <a className='ReadMore' href='https://jaxenter.de/tag/keine-angst-vor-javascript' target='_blank'><img src='/uploads/jaxenter.jpg'/>Keine Angst vor JavaScript! (JaxEnter-Kolumne)</a>
+                <a className='ReadMore' href='https://jaxenter.de/tag/keine-angst-vor-javascript' target='_blank'><img src='/uploads/jaxenter.jpg'/>Keine Angst vor JavaScript!(JaxEnter-Kolumne) </a>
               </div>
             </div>
+            <div className='Section'>
+              <h1 className='Title'>Beratung</h1>
+              <span className='ReadMore'>Gerne berate ich rund um das Thema JavaScript in Form von Schulungen, Workshops oder Coaching. Bei Interesse einfach <a href='mailto:nils@nilshartmann.net'>anfragen</a></span>
+            </div>
+
           </div>
         </div>
         <div className='Footer'>
@@ -71,15 +76,15 @@ class Layout extends React.Component {
   }
 }
 Layout.propTypes = {
-  children:      React.PropTypes.any,
+  children: React.PropTypes.any,
   authorization: React.PropTypes.string,
-  location:      React.PropTypes.object.isRequired,
-  logout:        React.PropTypes.func.isRequired,
-  newPost:       React.PropTypes.func.isRequired
+  location: React.PropTypes.object.isRequired,
+  logout: React.PropTypes.func.isRequired,
+  newPost: React.PropTypes.func.isRequired
 };
 
 const w = connectModel(Layout,
-  ({ location, authorization }) => ({location, authorization}),
-  ({ logout, newPost }) => ({logout, newPost}))
+  ({ location, authorization }) => ({ location, authorization }),
+  ({ logout, newPost }) => ({ logout, newPost }))
   ;
 export default w;
