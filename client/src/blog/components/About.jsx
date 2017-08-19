48142d94766290a9987bb26396931d25c6adfe10
Nils Hartmann <nils@nilshartmann.net>
2017-08-19 11:20:55 +0200

    Rework of About

--- client/src/blog/components/About.jsx
+++ client/src/blog/components/About.jsx
@@ -18,7 +18,11 @@ export default function About({ flickrImages }) {
   };
 
   const pStyle = {
-    marginBottom: "5px"
+    marginBottom: "8px"
+  };
+
+  const ulStyle = {
+    marginTop: 0
   };
 
   return (
@@ -27,22 +31,63 @@ export default function About({ flickrImages }) {
         <div className="TitlePanel Title ">Willkommen!</div>
 
         <p style={pStyle}>
-          <img style={imgStyle} src="/uploads/nh.jpg" />Ich bin Nils Hartmann, Software-Entwickler aus Hamburg.
-        </p>
-        <p style={pStyle}>
-          Hauptsächlich beschäftige ich mich it der Entwicklung von <b>JavaScript-Anwendungen</b> (insb mit <b>React</b>, Redux,
-          MobX, GraphQL und TypeScript). 2016 habe ich ein{" "}
+          <img style={imgStyle} src="/uploads/nh.jpg" />Ich bin Nils Hartmann, Softwareentwickler und -Architekt aus Hamburg und
+          beschäftige mich mit <b>JavaScript</b> (insbesondere <b>React</b>, Redux, GraphQL und <b>TypeScript</b>). 2016 habe ich
+          ein{" "}
           <a href="/posts/react-buch-erschienen">
             <b>Buch über React</b>
           </a>{" "}
-          veröffentlicht und biete <b>Schulungen und Workshops</b> an.
+          veröffentlicht.
         </p>
-        <p>
-          Da ich ursprünglich aus dem Java-Bereich (Spring, OSGi, JPA) komme, kann ich auch bei der Umstellung von Java auf
-          JavaScript behilflich sein. Wenn Du Interesse an einem Workshop hast, schreibe mir doch einfach eine{" "}
-          <a href="mailto:nils@nilshartmann.net">E-Mail</a>
+        <p style={pStyle}>
+          Rund um das Thema React und JavaScript biete ich <b>Trainings und Workshops</b> an. Da ich auch langjährige
+          Java-Erfahrung habe, kann ich auch bei der Umstellung von Java auf JavaScript behilflich sein. Wenn Du Interesse hast,
+          schreibe mir doch einfach eine <a href="mailto:nils@nilshartmann.net">E-Mail</a>.
         </p>
-        <p style={pStyle}>Viel Spaß auf dieser Seite!</p>
+        <p style={pStyle}>
+          Regelmäßig halte ich auch <b>Vorträge</b>:
+          <ul style={ulStyle}>
+            <li>
+              <a href="https://2017.javazone.no/program/5ddf7fcd3cdd41d78091f0677eb4eec0" target="_blank">
+                {" "}Overview of current (JavaScript) frontend architectures
+              </a>{" "}
+              (JavaZone, Oslo, September 2017)
+            </li>
+            <li>
+              <a
+                href="http://bed-con.org/2017/talks/Rethinking-Best-Practices--Moderne-Web-Anwendungen-mit-React-entwickeln"
+                target="_blank"
+              >
+                Rethinking Best Practices – Moderne Web-Anwendungen mit React entwickeln
+              </a>{" "}
+              (BedCon, Berlin, 22. September 2017)
+            </li>
+            <li>
+              <a href="https://www.oose.de/abendvortrag/single-page-anwendungen-mit-react/" target="_blank">
+                Single-Page-Anwendungen mit React
+              </a>{" "}
+              (OOSE Abendvortrag, Hamburg, 25. Oktober 2017)
+            </li>
+            <li>
+              <a href="https://jax.de/web-development-javascript/das-javascript-oekosystem-orientierungshilfen/" target="_blank">
+                Das JavaScript-Ökosystem: Orientierungshilfen
+              </a>{" "}
+              (W-Jax, München, 8. November 2017)
+            </li>
+            <li>
+              <a
+                href="https://jax.de/web-development-javascript/react-und-typescript-workshop-moderne-webanwendungen-entwickeln/"
+                target="_blank"
+              >
+                Workshop: Moderne Webanwendungen mit React und TypeScript entwickeln
+              </a>{" "}
+              (W-Jax, München, 10. November 2017)
+            </li>
+            <li>
+              <a href="https://nilshartmann.net/posts/vergangene-talks">...und weitere</a>
+            </li>
+          </ul>
+        </p>
       </div>
       <div className="Row">
         <FlickrBar flickrImages={flickrImages} />
